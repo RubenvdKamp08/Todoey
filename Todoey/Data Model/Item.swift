18bdef9c3b62737d46d87e922c9b845e2024960f
Ruben van de Kamp <ruben.van.de.kamp@guapa.nl>
2019-11-21 20:20:27 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ruben Van de Kamp on 21/11/2019.
+//  Copyright © 2019 Ruben Van de Kamp. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
